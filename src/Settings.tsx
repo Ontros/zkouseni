41ea16e9030bac8f616b2241a8f6e83f788959bc
Ontros <ontro512@gmail.com>
2021-11-30 19:20:45 +0100

    Dropdown fail

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -20,19 +20,21 @@ export default function Settings(props: Props) {
     }
 
     return (
-        <div >
+        <div className={'settings-container-main'}>
             <div className={`settings-opener settings-opener-${isOpen ? 'open' : 'closed'}`} onClick={onOpenerClick}>
                 <div className={`triangle-css triangle-${isOpen ? 'open' : 'closed'}`} ></div>
                 {`Nastavení`}
+            </div>
+            <div className={'settings-container'} style={{ display: isOpen ? "inline" : "none" }}>
+                <div className="from-to">
+                    <input type="number" className="num-selection" value={from} onChange={changeFrom} />
+                    <input type="number" className="num-selection" value={end} onChange={changeEnd} />
+                </div>
+                <div onChange={changeAskForSecond} className="radioContainer">
+                    <input type="radio" value="1" name="quest" className="radio" /> {questionaire.key1}
+                    <input type="radio" value="2" name="quest" className="radio" /> {questionaire.key2}
+                </div>
             </div >
-            <div className="from-to">
-                <input type="number" className="num-selection" value={from} onChange={changeFrom} />
-                <input type="number" className="num-selection" value={end} onChange={changeEnd} />
-            </div>
-            <div onChange={changeAskForSecond} className="radioContainer">
-                <input type="radio" value="1" name="quest" className="radio" /> {questionaire.key1}
-                <input type="radio" value="2" name="quest" className="radio" /> {questionaire.key2}
-            </div>
         </div>
     )
 }
